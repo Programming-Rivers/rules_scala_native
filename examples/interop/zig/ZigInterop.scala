54980f55d152679625e9a28ebf59e0f3e93d4ce1
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-02-21 16:16:17 +0000

    fixup zig interops

--- examples/interop/zig/ZigInterop.scala
+++ examples/interop/zig/ZigInterop.scala
@@ -3,16 +3,13 @@ package examples.interop.zig
 import scala.scalanative.unsafe.*
 
 @extern
-object ZigAdder {
+object ZigAdder:
   @name("add")
   def add(a: CInt, b: CInt): CInt = extern
-}
 
-object ZigInteropExample {
-  def main(args: Array[String]): Unit = {
+object ZigInteropExample:
+  def main(args: Array[String]): Unit =
     val a: CInt = 15
     val b: CInt = 27
     val result = ZigAdder.add(a, b)
     println(s"Using Zig: $a + $b = $result")
-  }
-}
