e8ad748d05525499822f0086babf7d2e7fe0a9c6
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-02-21 16:15:29 +0000

    fixup rust intorops

--- examples/interop/rust/RustInterop.scala
+++ examples/interop/rust/RustInterop.scala
@@ -3,16 +3,13 @@ package examples.interop.rust
 import scala.scalanative.unsafe.*
 
 @extern
-object RustAdder {
+object RustAdder:
   @name("add")
   def add(a: CInt, b: CInt): CInt = extern
-}
 
-object RustInteropExample {
-  def main(args: Array[String]): Unit = {
+object RustInteropExample:
+  def main(args: Array[String]): Unit =
     val a: CInt = 15
     val b: CInt = 27
     val result = RustAdder.add(a, b)
     println(s"Using Rust: $a + $b = $result")
-  }
-}
