4d520f42bfb9f6b95ef692a9bd96c73c73765aac
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-02-25 23:17:04 +0000

    docs(examples): remove the folder for old Scala Native features
    The new folder for basic examples is at examples/02-scala-native-features

--- examples/features/Intrinsics.scala
+++
@@ -1,9 +0,0 @@
-package examples.native
-
-import scala.scalanative.unsafe._
-import scala.scalanative.unsigned._
-
-@extern object stdlib:
-  def malloc(size: CSize): Ptr[Byte] = extern
-
-val ptr = stdlib.malloc(32.toCSize)
