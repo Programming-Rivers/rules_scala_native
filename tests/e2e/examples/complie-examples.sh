c2dde0e0c5db7de21da26ffa0f186ae82941531d
Meisam Rivers <meisam-on-github@googlegroups.com>
2026-02-26 03:53:49 +0000

    test(e2e): Build and run and test example projects when possible
    Check if the example project has an executable //:main,
    if so run it with `bazel run //:main`
    
    Chekc if the example has any tests.
    If so run the tests with `bazel test //...`

--- tests/e2e/examples/complie-examples.sh
+++ tests/e2e/examples/complie-examples.sh
@@ -6,21 +6,48 @@ readarray -t examples < <(find examples -name MODULE.bazel -exec dirname {} + | 
 echo "Found examples: "
 printf "* %s\n" "${examples[@]}"
 for example in "${examples[@]}"; do
-    echo "Bulding $example..."
+    echo "Building $example..."
     cd "$example"
-    echo "Cunnernt dir in: $PWD"
+    echo "Current dir in: $PWD"
     bazel build \
-      //...
-      --output_filter=warnings \
-      --noshow_progress
-    rc=$?
-    cd -
-    echo "Cunnernt dir out: $PWD"
+      --ui_event_filters=-INFO,-PROGRESS --noshow_progress \
+      //... \
+    && rc=0 || rc=$?
     if [ $rc -ne 0 ]; then
         echo "Build failed for $example with exit code $rc"
         exit $rc
     fi
     echo "successfully built $example"
+    # Run bazel test //... if there are any test targets
+    if [[ -n $(bazel --quiet cquery 'kind(test, //...)' 2>/dev/null) ]]; then
+        bazel test \
+          --ui_event_filters=-INFO,-PROGRESS --noshow_progress \
+          //... \
+        && rc=0 || rc=$?
+        if [ $rc -ne 0 ]; then
+            echo "Test failed for $example with exit code $rc"
+            exit $rc
+        fi
+        echo "successfully tested $example"
+    else
+        echo "Skipping tests for $example (no test targets found)"
+    fi
+    # Run bazel run //:main if it exists and it is executable
+    if [[ -n $(bazel --quiet cquery 'attr(executable, 1, //:main)' 2>/dev/null) ]]; then
+        bazel run \
+          --ui_event_filters=-INFO,-PROGRESS --noshow_progress \
+          //:main \
+        && rc=0 || rc=$?
+        if [ $rc -ne 0 ]; then
+            echo "Run failed for $example with exit code $rc"
+            exit $rc
+        fi
+        echo "successfully ran $example"
+    else
+        echo "Skipping run for $example (no executable //:main target)"
+    fi
+    cd -
+    echo "Current dir out: $PWD"
     echo "-----------------------------------------------------------------------"
     # exit 1
 done
